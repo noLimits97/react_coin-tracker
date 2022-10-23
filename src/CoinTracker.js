bcef57105180f515a25506d6e72165998f4a0a57
noLimits97 <ks971119@naver.com>
2022-10-23 23:57:10 +0900

    complete

--- src/CoinTracker.js
+++ src/CoinTracker.js
@@ -1,5 +1,50 @@
+import styles from "./CoinTracker.module.css";
+import { useState, useEffect } from "react";
+
 function CoinTracker() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const printH1 = (string) => {
+    return (
+      <h1>
+        <i>{string}</i>
+      </h1>
+    );
+  };
+  const getCoins = async () => {
+    const json = await (
+      await fetch(`https://api.coinpaprika.com/v1/tickers?limit=100`)
+    ).json();
+    setCoins(json);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getCoins();
+  }, []);
+  return (
+    <div>
+      {printH1("Coin Tracker!")}
+      {loading ? (
+        printH1("Loading...")
+      ) : (
+        <ul>
+          {Array.isArray(coins)
+            ? coins.map((coin) =>
+                coin.quotes.hasOwnProperty("USD") ? (
+                  <li className={styles.coin} key={coin.id}>
+                    {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+                  </li>
+                ) : (
+                  <li key={coin.id}>
+                    {coin.name} ({coin.symbol}): No Data
+                  </li>
+                )
+              )
+            : null}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default CoinTracker;
